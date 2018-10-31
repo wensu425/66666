0dce3f74661674ff73cf99d0cde464642dfa9bf6
wensu425 <s5wen@ucsd.edu>
2018-10-31 09:50:09 -0700

    Message

---
+++ src/edu/ucsd/cs110/temperature/Fahrenheit.java
@@ -0,0 +1,27 @@
+package edu.ucsd.cs110.temperature;
+
+public class Fahrenheit extends Temperature
+{
+    public Fahrenheit(float t)
+    {
+        super(t);
+    }
+
+    @Override
+    public Temperature toCelsius() {
+        float value = ((this.getValue()-32)/9)*5;
+        return new Celsius(value);
+
+    }
+
+    @Override
+    public Temperature toFahrenheit() {
+        return this;
+    }
+
+    public String toString()
+    {
+        // TODO: Complete this method
+        return this.getValue()+" F";
+    }
+}
